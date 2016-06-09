7e151da46efd6807dff150ba33e125ec2e62d689
hvelarde <hector.velarde@gmail.com>
2016-06-09 10:02:28 -0300

    Fix testing setup for Plone 5
    Also, add build for Plone 5.1

--- sc/social/like/testing.py
+++ sc/social/like/testing.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
+"""Setup testing infrastructure.
+
+For Plone 5 we need to install plone.app.contenttypes.
+"""
 from plone import api
 from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import z2
 
 import os.path
+import pkg_resources
+
+try:
+    pkg_resources.get_distribution('plone.app.contenttypes')
+except pkg_resources.DistributionNotFound:
+    from plone.app.testing import PLONE_FIXTURE
+else:
+    from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE as PLONE_FIXTURE
 
 IS_PLONE_5 = api.env.plone_version().startswith('5')
 
@@ -25,23 +36,20 @@ class Fixture(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
-        # Load ZCML
         import sc.social.like
         self.loadZCML(package=sc.social.like)
 
     def setUpPloneSite(self, portal):
-        # Install into Plone site using portal_setup
         self.applyProfile(portal, 'sc.social.like:default')
 
 FIXTURE = Fixture()
+
 INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,),
-    name='sc.social.like:Integration',
-)
+    bases=(FIXTURE,), name='sc.social.like:Integration')
+
 FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE,),
-    name='sc.social.like:Functional',
-)
+    bases=(FIXTURE,), name='sc.social.like:Functional')
+
 ROBOT_TESTING = FunctionalTesting(
     bases=(FIXTURE, AUTOLOGIN_LIBRARY_FIXTURE, z2.ZSERVER_FIXTURE),
     name='sc.social.like:Robot',
